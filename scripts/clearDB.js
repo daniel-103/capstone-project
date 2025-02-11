acaeda3c5c3ffb8c27abfc1edd62431e74b9b333
ctm042 <ctm042@latech.edu>
2025-02-11 10:38:32 -0600

    added script to clear db

---
+++ scripts/clearDB.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const userDataPath = path.join(process.env.APPDATA, 'capstone-project');
+
+fs.rm(userDataPath, { recursive: true, force: true }, (err) => {
+    if (err) {
+        console.error(`Error deleting pragram data: ${err}`);
+    } else {
+        console.log(`Successfully deleted program data at: ${userDataPath}`);
+    }
+});
