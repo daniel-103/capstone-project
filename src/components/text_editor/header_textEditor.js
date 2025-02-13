687af3f56e6fb0d7174d154fc1fe12314145d6f1
sjohn65189 <70227912+sjohn65189@users.noreply.github.com>
2025-02-13 00:33:48 -0600

    made the cut, copy, and paste functionality

--- src/components/text_editor/header_textEditor.js
+++ src/components/text_editor/header_textEditor.js
@@ -5,3 +5,19 @@ window.top.document.getElementById('edit-undo-btn').addEventListener('click', ()
 
 // Redo
 window.top.document.getElementById('edit-redo-btn').addEventListener('click', () => quill.history.redo());
+
+// Cut
+window.top.document.getElementById('edit-cut-btn').addEventListener('click', () => {
+    document.execCommand('cut');
+});
+
+// Copy
+window.top.document.getElementById('edit-copy-btn').addEventListener('click', () => {
+    document.execCommand('copy');
+});
+
+// Paste
+window.top.document.getElementById('edit-paste-btn').addEventListener('click', () => {
+    document.execCommand('paste');
+});
+
