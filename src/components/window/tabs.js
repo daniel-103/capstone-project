2280d34fd4062ed0ba482a5f42d795c8728cfc93
ctm042 <ctm042@latech.edu>
2025-03-21 18:24:51 -0500

    added global function to switch tabs by page id

--- src/components/window/tabs.js
+++ src/components/window/tabs.js
@@ -1,4 +1,4 @@
-window.tabs = [];
+window.top.tabs = window.top.tabs || [];
 
 window.currTabIndex = 0
 
@@ -14,26 +14,25 @@ function runTabs() {
 
     addNewTab(`Tab 1`, startPagePath, tabHeader, pageWindow);
 
-    initPlusButton(tabHeader, pageWindow);
+    // initPlusButton(tabHeader, pageWindow);
 }
 
 window.defaultPagePath = defaultPagePath;
 window.addNewTab = addNewTab;
 
 // Plus button should be deprecated at some point now that the new file button in the hierarchy has been implemented.
-function initPlusButton(tabHeader, pageWindow) {
-    window.plusButton = document.createElement("button");
-    plusButton.textContent = "+";
-    plusButton.addEventListener("click", () => {
-        addNewTab(`Tab ${tabs.length+1}`, defaultPagePath, tabHeader, pageWindow);
-    });
+// function initPlusButton(tabHeader, pageWindow) {
+//     window.plusButton = document.createElement("button");
+//     plusButton.textContent = "+";
+//     plusButton.addEventListener("click", () => {
+//         addNewTab(`Tab ${tabs.length+1}`, defaultPagePath, tabHeader, pageWindow);
+//     });
 
-    tabHeader.appendChild(plusButton);
-}
+//     tabHeader.appendChild(plusButton);
+// }
 
 function createTab(index, tabHeader, pageWindow) {
-    
-    const currTab = tabs[index];
+    const currTab = window.top.tabs[index];
 
     const tabContainer = document.createElement("div");
     tabContainer.classList.add("tab");
@@ -43,7 +42,7 @@ function createTab(index, tabHeader, pageWindow) {
     tabButton.textContent = currTab.title;
     
     tabButton.addEventListener("click", () => {
-        const currIndex = tabs.indexOf(currTab);
+        const currIndex = window.top.tabs.indexOf(currTab);
         switchTab(currIndex);
     });
 
@@ -52,7 +51,7 @@ function createTab(index, tabHeader, pageWindow) {
     closeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>';
     closeButton.addEventListener("click", (e) => {
         e.stopPropagation();
-        const currIndex = tabs.indexOf(currTab);
+        const currIndex = window.top.tabs.indexOf(currTab);
         removeTab(currIndex);
     });
 
@@ -89,13 +88,40 @@ function createTab(index, tabHeader, pageWindow) {
 }
 
 function switchTab(index) {
-    tabs[currTabIndex].iframeElem.style.display = "none";
-    tabs[currTabIndex].containerElem.classList.remove("active");
+window.top.tabs[currTabIndex].iframeElem.style.display = "none";
+    window.top.tabs[currTabIndex].containerElem.classList.remove("active");
 
-    tabs[index].iframeElem.style.display = "block";
-    tabs[index].containerElem.classList.add("active");
+    window.top.tabs[index].iframeElem.style.display = "block";
+    window.top.tabs[index].containerElem.classList.add("active");
 
     currTabIndex = index;
+}
+
+window.top.switchTabById = async (id) => {
+    // switch to tab if it's open
+    // I know addNewTab already does this so I could just call it without checking but the page would need to be fetched every time.
+    // Checking if a page already exists with the same id prevent any unnecessary fetches.
+    const tabIndex = window.top.tabs.findIndex(tab => tab.id == id);
+    if (tabIndex >= 0) {
+        switchTab(tabIndex);
+        return;
+    }
+
+    // if not, fetch page and create it
+    // const tabHeader = document.getElementById("tab-header");
+    const mainWindow = window.top.document.getElementById("window").contentDocument
+    const tabHeader = mainWindow.getElementById("tab-header");
+    const pageWindow = mainWindow.getElementById("page-window");
+    window.top.db.get(id)
+        .then(page => {
+            const path = window.top.pagePaths[page.fileType] + "?id=" + encodeURIComponent(page._id)
+            addNewTab(page.name, path, tabHeader, pageWindow)
+        })
+
+    // const page = await window.top.db.get(id);
+    // const path = window.top.pagePaths[page.fileType] + "?id=" + encodeURIComponent(page._id)
+    // addNewTab(page.name, path, tabHeader, pageWindow)
+
 }
 
 function addNewTab(title, path, tabHeader, pageWindow) {
@@ -103,37 +129,35 @@ function addNewTab(title, path, tabHeader, pageWindow) {
     const urlParams = new URLSearchParams(query);
     const id = urlParams.get("id");
     const newTabInfo = { title, path, id };
-    const newIndex = tabs.length;
+    const newIndex = window.top.tabs.length;
     
     // Switch to tab if it already exists
     if (id) {
-        console.log(id);
-        const index = tabs.findIndex((tab) => tab.id === id);
+        const index = window.top.tabs.findIndex((tab) => tab.id === id);
         if (index != -1) {
             switchTab(index);
             return;
         }
     }
 
-    tabs.push(newTabInfo);
+    window.top.tabs.push(newTabInfo);
 
-    createTab(newIndex, tabHeader, pageWindow) 
+    createTab(newIndex, tabHeader, pageWindow)
     switchTab(newIndex);
 }
 
 function removeTab(index) {
-    // console.log(index);
-    const tabInfo = tabs[index];
+    const tabInfo = window.top.tabs[index];
 
     tabInfo.containerElem.remove();
     tabInfo.iframeElem.remove();
 
-    tabs.splice(index, 1);
+    window.top.tabs.splice(index, 1);
 
     if (index <= currTabIndex) {
         currTabIndex = Math.max(0, currTabIndex-1);
     }
-    if (tabs.length > 0) {
+    if (window.top.tabs.length > 0) {
         switchTab(currTabIndex);
     }
     
