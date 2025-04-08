20e15ab39afc507a1d1ff7847801fd3022121784
sjohn65189 <70227912+sjohn65189@users.noreply.github.com>
2025-04-07 21:52:34 -0500

    updating current projects notification modal
    With the new modal, it can now do alerts for duplicating and deleting

--- src/components/template/projects.js
+++ src/components/template/projects.js
@@ -458,10 +458,69 @@ async function initProjects() {
               childrenIds: newChildrenIds
             };
             await window.top.db.put(updatedProject); // Save the updated project
-          }
 
-          location.reload(); // Reload the project hub to reflect the new project
-          showNotification(`Project "${project.name}" duplicated successfully.`, 'success');
+            const newCard = document.createElement('div');
+            newCard.className = 'project-card';
+            newCard.setAttribute('projectId', updatedProject._id);
+            
+            // Check if the image is a data URL or a file path
+            const newImageUrl = updatedProject.image.startsWith('data:') 
+              ? updatedProject.image // Use the data URL directly
+              : `../../assets/images/${updatedProject.image}`; // Construct the file path for regular images
+        
+            newCard.innerHTML = `
+              <div class="template-image" style="background-image: url('${newImageUrl}');"></div>
+              <div class="template-title">${updatedProject.name}</div>
+              <div class="template-description">${updatedProject.description}</div>
+        
+              <div class="toolbar">
+                <div class="button-rack">
+                  <button title="Open ${updatedProject.name}" class="btn-open">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M88.7 223.8L0 375.8 0 96C0 60.7 28.7 32 64 32l117.5 0c17 0 33.3 6.7 45.3 18.7l26.5 26.5c12 12 28.3 18.7 45.3 18.7L416 96c35.3 0 64 28.7 64 64l0 32-336 0c-22.8 0-43.8 12.1-55.3 31.8zm27.6 16.1C122.1 230 132.6 224 144 224l400 0c11.5 0 22 6.1 27.7 16.1s5.7 22.2-.1 32.1l-112 192C453.9 474 443.4 480 432 480L32 480c-11.5 0-22-6.1-27.7-16.1s-5.7-22.2 .1-32.1l112-192z"/></svg>
+                  </button>
+                  <button title="About ${updatedProject.name}" class="btn-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32l64 0c17.7 0 32 14.3 32 32l0 224 32 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 512c-17.7 0-32-14.3-32-32s14.3-32 32-32l32 0 0-192-32 0c-17.7 0-32-14.3-32-32z"/></svg>
+                  </button>
+                  <button title="Change Image for ${updatedProject.name}" class="btn-change-image">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path d="M464 448H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h416c26.51 0 48 21.49 48 48v288c0 26.51-21.49 48-48 48zM128 192c-17.67 0-32 14.33-32 32s14.33 32 32 32 32-14.33 32-32-14.33-32-32-32zm320 160L352 288l-96 128H64l128-160 96 128 64-96 96 128z"/>
+                    </svg>
+                  </button>
+                  <button title="Rename Project Title for ${updatedProject.name}" class="btn-rename-title">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M.1 29.3C-1.4 47 11.7 62.4 29.3 63.9l8 .7C70.5 67.3 96 95 96 128.3L96 224l-32 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l32 0 0 95.7c0 33.3-25.5 61-58.7 63.8l-8 .7C11.7 449.6-1.4 465 .1 482.7s16.9 30.7 34.5 29.2l8-.7c34.1-2.8 64.2-18.9 85.4-42.9c21.2 24 51.2 40 85.4 42.9l8 .7c17.6 1.5 33.1-11.6 34.5-29.2s-11.6-33.1-29.2-34.5l-8-.7C185.5 444.7 160 417 160 383.7l0-95.7 32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-32 0 0-95.7c0-33.3 25.5-61 58.7-63.8l8-.7c17.6-1.5 30.7-16.9 29.2-34.5S239-1.4 221.3 .1l-8 .7C179.2 3.6 149.2 19.7 128 43.7c-21.2-24-51.2-40-85.4-42.9l-8-.7C17-1.4 1.6 11.7 .1 29.3z"/></svg>  
+                  </button>
+                  <button title="Rename Description for ${updatedProject.name}" class="btn-rename-description">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path d="M290.74 93.24a32 32 0 0 0-45.25 0l-224 224a32 32 0 0 0-8.19 13.7L.06 482.3a16 16 0 0 0 19.64 19.64l151.36-13.24a32 32 0 0 0 13.7-8.19l224-224a32 32 0 0 0 0-45.25ZM124.12 422.06l-92.18 8.06 8.06-92.18 176-176 84.12 84.12ZM478.33 33.67a96 96 0 0 0-135.66 0l-56.56 56.56 135.66 135.66 56.56-56.56a96 96 0 0 0 0-135.66ZM403.88 108.12l-84.12-84.12 33.94-33.94a64 64 0 0 1 90.5 90.5Z"/>
+                    </svg>
+                  </button>
+                  <button title="Duplicate ${updatedProject.name}" class="btn-duplicate">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 448L64 448l0-224 64 0 0-64-64 0c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l224 0c35.3 0 64-28.7 64-64l0-64-64 0 0 64zm-64-96l224 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L224 0c-35.3 0-64 28.7-64 64l0 224c0 35.3 28.7 64 64 64z"/></svg>
+                  </button>
+                  <button title="Delete ${updatedProject.name}" class="btn-delete">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+                  </button>
+                </div>
+        
+                <div class="delete-confirmation">
+                  <p>Confirm?</p>
+                    <button title="Cancel Deletion" class="btn-delete-cancel">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg>
+                    </button>
+                </div>
+              </div>
+            `;
+            
+            // Add event listeners for the new card (similar to the existing ones)
+            newCard.onclick = () => {
+              localStorage.setItem('projectId', updatedProject._id);
+              const windowIframe = window.parent.document.getElementById('window');
+              windowIframe.src = 'components/window/window.html';
+            };
+
+            project_gallery.appendChild(newCard);
+            showNotification(`Project "${updatedProject.name}" duplicated successfully.`, 'success');
+          }
         } else {
           throw new Error('Failed to duplicate project.');
         }
@@ -552,6 +611,7 @@ async function initProjects() {
               // TODO: Recursively delete all nested files and folders
               if (window.top.DEBUG) console.log(`✅ [2] Project "${project.name}" deleted.`);
               card.remove();
+              showNotification(`Project "${project.name}" deleted successfully.`, 'success');
 //              location.reload();
             })
           .catch(error => {
