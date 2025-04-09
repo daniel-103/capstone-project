6ba1a155b10fdcb745c48d94185478e04b965381
daniel-103 <djr035@email.latech.edu>
2025-04-09 16:20:34 -0500

    Fixed initial db update bug

--- src/components/window/createTypeInfo.js
+++ src/components/window/createTypeInfo.js
@@ -2,10 +2,17 @@ import templateData from "../template/template_types/emptyTemplate.js";
 import getFileOptions from "../template/getFileOptions.js"
 // this ensures that a doc with ID entityTypes always exists (when you open the app)
 async function initEntityTypes() {
-    
     try {
         const entityTypes = await window.top.db.get('entityTypes');
+        if (entityTypes.error) {
+            await addEntityTypes();
+        }
     } catch(err) {
+        await addEntityTypes();
+    }
+}
+
+async function addEntityTypes() {
         let someTypes = await getFileOptions(templateData);
         someTypes = JSON.stringify(someTypes);
 
@@ -13,8 +20,8 @@ async function initEntityTypes() {
             _id: 'entityTypes',
             entityTypes: someTypes
         }
-        await window.top.db.put(typeInfo);
-    }
+        const result = await window.top.db.put(typeInfo);
+        console.log("result: ", result);
 }
 
 await initEntityTypes();
