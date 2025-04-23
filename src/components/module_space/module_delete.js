e21827fc32896d93cf5ea03fcaf71b416aac896d
daniel-103 <djr035@email.latech.edu>
2025-04-23 13:08:33 -0500

    Added delete module confirmation

--- src/components/module_space/module_delete.js
+++ src/components/module_space/module_delete.js
@@ -15,8 +15,14 @@ function deleteModule(module, entityData) {
     const moduleType = module.dataset.moduleKey;
     if (!module.classList.contains("selected")) return;
     
+    if (!confirmDelete()) return;
+
     entityData.modules = entityData.modules.filter(module => module.type !== moduleType);
 
     module.remove();
     addSelect();
+}
+
+function confirmDelete() {
+    return window.confirm('Are you sure you want to delete this item? This action cannot be undone.');
 }
